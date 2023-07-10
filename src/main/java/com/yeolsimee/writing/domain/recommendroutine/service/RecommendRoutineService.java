7a5a36ef3eb28d4e99ac4547a868ec97c75bcc6c
Sangmin <sm9171@nate.com>
2023-07-10 16:50:51 +0900

    feat: 추천루틴 등록 controller 구현, header 추천루틴 등록 탭 추가

--- src/main/java/com/yeolsimee/writing/domain/recommendroutine/service/RecommendRoutineService.java
+++ src/main/java/com/yeolsimee/writing/domain/recommendroutine/service/RecommendRoutineService.java
@@ -2,6 +2,7 @@ package com.yeolsimee.writing.domain.recommendroutine.service;
 
 import com.yeolsimee.writing.domain.recommendroutine.entity.RecommendRoutine;
 import com.yeolsimee.writing.domain.recommendroutine.repository.RecommendRoutineRepository;
+import com.yeolsimee.writing.web.recommendroutine.dto.InsertRecommendRoutineDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,4 +18,11 @@ public class RecommendRoutineService {
     public Page<RecommendRoutine> findAll(Pageable pageable) {
         return recommendRoutineRepository.findAll(pageable);
     }
+
+	@Transactional
+	public RecommendRoutine createRecommendRoutine(InsertRecommendRoutineDto insertRecommendRoutineDto) {
+		//todo: 추천루틴 생성 로직
+
+		return null;
+	}
 }
--- src/main/java/com/yeolsimee/writing/web/recommendroutine/controller/RecommendRoutineController.java
+++ src/main/java/com/yeolsimee/writing/web/recommendroutine/controller/RecommendRoutineController.java
@@ -1,15 +1,25 @@
 package com.yeolsimee.writing.web.recommendroutine.controller;
 
+import com.yeolsimee.writing.domain.recommendroutine.entity.RecommendRoutine;
+import com.yeolsimee.writing.domain.recommendroutine.service.RecommendRoutineService;
 import com.yeolsimee.writing.web.recommendroutine.dto.InsertRecommendRoutineDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class RecommendRoutineController {
 
-
+	private final RecommendRoutineService recommendRoutineService;
 
 	@GetMapping("/recommendroutine")
 	public String getRecommendRoutineForm(Model model) {
@@ -18,9 +28,18 @@ public class RecommendRoutineController {
 	}
 
 	@PostMapping("/recommendroutine")
-	public String createRecommendRoutine(Model model) {
-		//todo: 추천루틴 생성 로직
+	public String createRecommendRoutine(@Validated @ModelAttribute("insertRecommendRoutineDto") InsertRecommendRoutineDto insertRecommendRoutineDto,
+	                                     BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
-		return "recommendroutine/registerform";
+		try {
+			RecommendRoutine savedRecommendRoutine = recommendRoutineService.createRecommendRoutine(insertRecommendRoutineDto);
+			redirectAttributes.addAttribute("itemId", savedRecommendRoutine.getId());
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			bindingResult.reject("globalError", "추천 루틴 등록 중 에러가 발생하였습니다.");
+			return "recommendroutine/registerform";
+		}
+
+		return "main/mainpage";
 	}
 }
